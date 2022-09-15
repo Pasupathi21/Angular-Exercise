2992419b2d1c9df82eac75c4ba67d77505f0b9f3
Pasupathi M <pasupathi@aaludra.com>
2022-09-15 23:27:04 +0530

    Ngrx package added

--- test-state/src/app/app.module.ts
+++ test-state/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ActionComponent } from './action-color/action/action.component';
+import { ColorComponent } from './action-color/color/color.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ActionComponent,
+    ColorComponent
   ],
   imports: [
     BrowserModule
